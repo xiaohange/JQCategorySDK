6482e69974f214426f29356b6ed821466d2e509c
HaRi <532167805@qq.com>
2016-11-08 10:40:32 +0800

    0.0.2

--- JQCategorySDK.framework/Headers/JQCategorySDK.h
+++ JQCategorySDK.framework/Headers/JQCategorySDK.h
@@ -15,5 +15,16 @@ FOUNDATION_EXPORT double JQCategorySDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char JQCategorySDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JQCategorySDK/PublicHeader.h>
+#import "CALayer+GCAdd.h"
+#import "NSDate+GCAdd.h"
+#import "NSString+GCAdd.h"
+#import "NSString+MD5.h"
+#import "UIButton+GCAlignment.h"
+#import "UIImage+GCAdd.h"
+#import "UIScrollView+GCAdd.h"
+#import "UITabBar+badge.h"
+#import "UITextField+removeDelegate.h"
+#import "UIView+GCAdd.h"
+
 
 
